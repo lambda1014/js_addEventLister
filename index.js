b362e29dcbe1be91e419319e2ee8a566015e6cd8
lambda1014 <69205195+lambda1014@users.noreply.github.com>
2020-10-23 17:22:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+//import "./styles.css";
 const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-// <⚠️ /DONT DELETE THIS ⚠️>
+
 
 const title = document.querySelector("h2");
 
